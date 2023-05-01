01fbecd0e5dcccf409961f4f5bc2f4af97f60852
[Kostiantyn] <[worldairsport@gmail.com]>
2023-05-02 00:52:39 +0300

    fix: mouseClick function replacement

--- script.js
+++ script.js
@@ -55,82 +55,92 @@ function full() {
       mouseClick(event.target);
     });
   }
+  // const spaceButtonIndex = symbols.findIndex(
+  //   (symbol) => symbol.button === "58"
+  // );
 
-  const spaceButtonIndex = symbols.findIndex(
-    (symbol) => symbol.button === "58"
-  );
-
-  button[58].addEventListener("click", function () {
-    textareaElement.append(" ");
-  });
-  if (capsLock == 1) {
-    buttons[29].classList.add("active-keys");
-  }
+  // button[58].addEventListener("click", function () {
+  //   textareaElement.append(" ");
+  // });
+  // if (capsLock == 1) {
+  //   buttons[29].classList.add("active-keys");
+  // }
 }
 full();
 
-function mouseClick(button) {
-  let attribute = button.getAttribute("data");
-  switch (attribute) {
-    case "29": // Caps Lock
-      button.classList.toggle("active-keys");
-      capsLock = capsLock === 0 ? 1 : 0;
-      button = "";
-      full();
-      break;
-    case "58": // Space
-      textareaElement.append(" ");
-      break;
-    case "14": //Tab
-      textareaElement.append("    ");
-      break;
-    case "13": // Backspace
-      textareaElement.append(Backspace());
-      break;
-    case "28": // Delete
-      textareaElement.append(Delete());
-      break;
-    case "57":
-    case "59":
-    case "55":
-    case "56":
-    case "60": // Empty value
-      textareaElement.append("");
-      break;
-    case "41": // Enter
-      textareaElement.append("\n");
-      break;
-    case "53": // Arrow Up
-      textareaElement.append(ArrowUp());
-      break;
-    case "61": // Arrow Left
-      textareaElement.append(ArrowLeft());
-      break;
-    case "62": // Arrow Down
-      textareaElement.append(ArrowDown());
-      break;
-    case "63": // Arrow Right
-      textareaElement.append(ArrowRight());
-      break;
-    case "42":
-    case "54": // Arrow Right
-      const shiftActive =
+let activeShift;
+
+function mouseClick(clicked) {
+  let attribute = clicked.getAttribute("data");
+  if (attribute == "29") {
+    clicked.classList.toggle("active-keys");
+    if (capsLock == 0) {
+      capsLock = 1;
+    } else if (capsLock == 1) {
+      capsLock = 0;
+    }
+    button = "";
+    full();
+  } else if (attribute == "13") {
+    textareaElement.append(Backspace());
+  } else if (attribute == "14") {
+    textareaElement.append(Tab());
+  } else if (attribute == "28") {
+    textareaElement.append(Delete());
+  } else if (attribute == "41") {
+    textareaElement.append(Enter());
+  } else if (attribute == "53") {
+    textareaElement.append(ArrowUp());
+  } else if (attribute == "58") {
+    textareaElement.append(Space());
+  } else if (attribute == "61") {
+    textareaElement.append(ArrowLeft());
+  } else if (attribute == "62") {
+    textareaElement.append(ArrowDown());
+  } else if (attribute == "63") {
+    textareaElement.append(ArrowRight());
+  } else if (
+    ((attribute == "57" || attribute == "59") &&
+      (document
+        .querySelector('.buttons[data="42"]')
+        .classList.contains("active") ||
         document
-          .querySelector('.buttons[data="57"]')
-          .classList.contains("active") ||
+          .querySelector('.buttons[data="54"]')
+          .classList.contains("active"))) ||
+    ((attribute == "42" || attribute == "54") &&
+      (document
+        .querySelector('.buttons[data="57"]')
+        .classList.contains("active") ||
         document
           .querySelector('.buttons[data="59"]')
-          .classList.contains("active");
-      if (shiftActive) {
-        toggleLanguage();
-      }
-      break;
-    case "42":
-    case "54":
-      textareaElement.append("");
-      break;
-    default: // Character key
-      textareaElement.append(symbols[attribute].eng[capsLock]);
+          .classList.contains("active")))
+  ) {
+    textareaElement.append("");
+    if (lang == "eng") {
+      localStorage.setItem("Language", "rus");
+      lang = "rus";
+    } else if (lang == "rus") {
+      localStorage.setItem("Language", "eng");
+      lang = "eng";
+    }
+    if (
+      document.querySelector('.buttons[data="42"]').classList.contains("active")
+    )
+      activeShift = "42";
+    if (
+      document.querySelector('.buttons[data="54"]').classList.contains("active")
+    )
+      activeShift = "54";
+    button = "";
+    fill();
+  } else if (["42", "54", "55", "56", "57", "59", "60"].includes(attribute)) {
+    textareaElement.append("");
+  } else {
+    if (lang == "rus") {
+      textareaElement.append(Else(symbols[attribute].rus[capsLock]));
+    } else {
+      textareaElement.append(Else(symbols[attribute].eng[capsLock]));
+    }
   }
 }
 
